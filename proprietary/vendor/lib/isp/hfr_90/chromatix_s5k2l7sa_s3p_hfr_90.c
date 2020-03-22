e4a06773ee199170cf168e85cd34e8ae2cf7e7bf
AndroidBlobs <AndroidBlobs@github.com>
2020-03-22 00:28:31 +0000

    HH6_00WW_FIH-user 10 QKQ1.190828.002 FS8032T0305Q release-keys
    Signed-off-by: AndroidBlobs <AndroidBlobs@github.com>

---
+++ proprietary/vendor/lib/isp/hfr_90/chromatix_s5k2l7sa_s3p_hfr_90.c
@@ -0,0 +1,26 @@
+/*============================================================================
+
+Copyright (c) 2016 Qualcomm Technologies, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Qualcomm Technologies, Inc.
+
+============================================================================*/
+
+/*============================================================================
+ *                      INCLUDE FILES
+ *===========================================================================*/
+#include "chromatix.h"
+
+static chromatix_parms_type chromatix_s5k2l7sa_s3p_parms = {
+#include "chromatix_s5k2l7sa_s3p_hfr_90.h"
+};
+
+/*============================================================================
+ * FUNCTION    - load_chromatix -
+ *
+ * DESCRIPTION:
+ *==========================================================================*/
+void *load_chromatix(void)
+{
+  return &chromatix_s5k2l7sa_s3p_parms;
+}
